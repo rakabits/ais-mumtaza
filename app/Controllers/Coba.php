e93d9ca6639afbcabdd8af891725f2ddbcd5457e
M Galih Rakasiwi <rakaalbantani23@gmail.com>
2023-02-17 00:52:12 +0700

    first commit

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        return view('welcome_message');
+    }
+
+    public function coba()
+    {
+        echo "YUK NGOPI!";
+    }
+
+    public function team()
+    {
+        echo "Halo, kenalkan kita $this->team";
+    }
+}

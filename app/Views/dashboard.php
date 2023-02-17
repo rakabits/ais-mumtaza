799f92fcf0827737b6f1293fdc7dddcab96344ee
M Galih Rakasiwi <rakaalbantani23@gmail.com>
2023-02-17 08:01:59 +0700

    update silde bar
    menambakan menu tambah guru dan siswa

--- app/Views/dashboard.php
+++ app/Views/dashboard.php
@@ -409,6 +409,18 @@
         </li>
         <li class="nav-item">
           <a class="nav-link" href='home'>
+            <i class="menu-icon mdi mdi-account-plus"></i>
+            <span class="menu-title">Tambah Guru</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href='home'>
+            <i class="menu-icon mdi mdi-account-multiple-plus"></i>
+            <span class="menu-title">Tambah Siswa</span>
+          </a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href='home'>
             <i class="menu-icon mdi mdi-checkbox-marked-outline"></i>
             <span class="menu-title">Absen Santri</span>
           </a>
@@ -475,27 +487,6 @@
           <div class="col-sm-12">
             <div class="home-tab">
               <div class="d-sm-flex align-items-center justify-content-between border-bottom">
-                <ul class="nav nav-tabs" role="tablist">
-                  <li class="nav-item">
-                    <a class="nav-link active ps-0" id="home-tab" data-bs-toggle="tab" href="#overview" role="tab" aria-controls="overview" aria-selected="true">Overview</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="profile-tab" data-bs-toggle="tab" href="#audiences" role="tab" aria-selected="false">Audiences</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" id="contact-tab" data-bs-toggle="tab" href="#demographics" role="tab" aria-selected="false">Demographics</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link border-0" id="more-tab" data-bs-toggle="tab" href="#more" role="tab" aria-selected="false">More</a>
-                  </li>
-                </ul>
-                <div>
-                  <div class="btn-wrapper">
-                    <a href="#" class="btn btn-otline-dark align-items-center"><i class="icon-share"></i> Share</a>
-                    <a href="#" class="btn btn-otline-dark"><i class="icon-printer"></i> Print</a>
-                    <a href="#" class="btn btn-primary text-white me-0"><i class="icon-download"></i> Export</a>
-                  </div>
-                </div>
               </div>
               <div class="tab-content tab-content-basic">
                 <div class="tab-pane fade show active" id="overview" role="tabpanel" aria-labelledby="overview">
